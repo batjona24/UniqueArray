3d3b40bec522823196df456ce5208437053d859a
batjona24 <88026430+batjona24@users.noreply.github.com>
2022-11-28 23:50:34 +0100

    I create all the files needed and a function to find the unique value of an array.

---
+++ index.js
@@ -0,0 +1,8 @@
+import fsp from 'fs/promises'
+async function UniqueArray(arr){
+    const data = await fsp.readFile('./data.csv', 'utf8') 
+    var arr = data.split(/\r?\n/);  
+    let unique = arr.filter((item, i, ar) => ar.indexOf(item) === i);
+    console.log(unique);      
+}
+await UniqueArray('./data.csv')
